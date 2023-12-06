202503f206d919cd5d5f07716d91c2597386e06f
BINEARY-BEAT <153111871+BINEARY-BEAT@users.noreply.github.com>
2023-12-06 22:05:58 +0500

    CREATED PYTHON FILE FOR BASIC ARITHMETIC CALCULATIONS

---
+++ CALCULATOR.py
@@ -0,0 +1,136 @@
+import time
+import os
+
+while True:
+
+
+ def ADDATION(a,b):
+     return a+b
+
+ def SUBTRACTION(a,b):
+     return a-b
+
+ def MULTIPLICATION(a,b):
+     return a*b
+
+ def DIVISION(a,b):
+     if b==0:
+         return " INFINITY"
+     return a/b
+
+ def FIND_MAX(a,b,c):
+     if a >=b  and a>c:
+         return("\nA is maximum!\n")
+     elif b>=a and b>=c:
+         return("\nB is maximum \n")
+     else:
+         return("\nC is  maximum\n")
+    
+ def EVEN(a,b):
+     if a%2==0 and b%2==0:
+         return ("\nA and B boath are Even\n")
+     elif a%2 != 0 and b%2 !=0:
+         return ("\n A and B boath are Odd\n")
+     elif a%2==0:
+         return ("\nA is Even and B is Odd\n")
+     else:
+         return ("\nB is Odd and A is Even\n")
+
+ def FACTORIAL (a):
+     if a == 0:
+         return 1
+     else:
+         fictorial=1
+         for i in range (1,a+1):
+             fictorial=fictorial*i
+     return fictorial
+ 
+ def SRAURE(a):
+      return a**2
+
+ def CUBE(a):
+     return a**3
+
+ def POWER(Base,Exponent):
+     return Base** Exponent
+
+ CHOICE=int(input('''Enter index of any Operation To Do...\n\n
+   1-ADDATION 
+   2-SUBTRACTION
+   3-MULTIPLICATION
+   4-DIVISION
+   5-MAXIMUM NUMBER CALCULATE
+   6-EVEN ODD CHECK
+   7-FACTORIAL
+   8-SQAURE
+   9-CUBE
+   10-POWER
+  :\n   
+'''))
+
+ if CHOICE ==1:
+     a=int(input("\nEnter The Value Of A\n:"))
+     b=int(input("\nEnter The Value Of B\n:"))
+     result=ADDATION(a,b)
+     print("\nSum Of Given numbers is : ",result)
+
+ elif CHOICE ==2:
+     a=int(input("\nEnter The Value Of A\n:"))
+     b=int(input("\nEnter The Value Of B\n:"))
+     result=SUBTRACTION(a,b)
+     print(f"\n{a}-{b} is ={result}")
+    
+ elif CHOICE ==3:
+     a=int(input("\nEnter The Value Of A\n:"))
+     b=int(input("\nEnter The Value Of B\n:"))
+     result=MULTIPLICATION(a,b)
+     print(f"\n {a}x{b}={result} ")
+    
+ elif CHOICE==4:
+     a=int(input("\nEnter The Value Of A\n:"))
+     b=int(input("\nEnter The Value Of B\n:"))
+     result=DIVISION(a,b)
+     print(f"{a}/{b}={result}")
+
+ elif CHOICE==5:
+     a=int(input("\nEnter The Value Of B\n:"))
+     b=int(input("\nEnter The Value Of B\n:"))
+     c=int(input("\nEnter The Value Of C\n:"))
+     result=FIND_MAX(a,b,c)
+     print("The Maximum Number is ",result)
+
+ elif CHOICE==6:
+     a=int(input("\nEnter The Value Of A\n:"))
+     b=int(input("\nEnter The Value Of B\n:"))
+     result=EVEN(a,b)
+     print(result)
+
+ elif CHOICE==7:
+     a=int(input("\nEnter The Value Of A\n:"))
+     result=FACTORIAL(a)
+     print("\nFactorial of Given Number is : ",result)
+
+ elif CHOICE ==8:
+     a=int(input("\nEnter The Value Of A\n:"))
+     result=SRAURE(a)
+     print("\nThe Sqaure Of Given Number is : ",result)
+
+ elif CHOICE ==9:
+     a=int(input("\nEnter The Value Of A\n:"))
+     result=CUBE(a)
+     print("\nThe Cube Of Given Number is :",result)
+
+ elif CHOICE ==10:
+     Base=int(input("\nEnter The Value Of Base\n:"))
+     Exponent=int(input("\nEnter The Value Of Exponent\n:"))
+     result=POWER(Base,Exponent)
+     print(f"{Base} To The Power Of {Exponent} is : {result}")
+
+ Choice = input("\nDo You Want Make another Calculation[Y/N]\n:")
+ if Choice == 'y' or Choice=='Y':
+     continue
+ else:
+     print("\nExiting The Calculator....\n")
+     break
+time.sleep(3)
+os.system("cls")
